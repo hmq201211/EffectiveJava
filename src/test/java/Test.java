46f92a61e680961a968086834c7a40409bfe3640
hmq201211 <879602728@qq.com>
2020-12-03 17:29:09 +0800

    20201203

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,8 @@
 import Requirement2.NutritionFacts;
+import org.junit.Assert;
+
+import java.util.HashMap;
+import java.util.Set;
 
 public class Test {
     @org.junit.Test
@@ -6,5 +10,15 @@ public class Test {
         NutritionFacts.Builder builder = new NutritionFacts.Builder(2, 100);
         NutritionFacts build = builder.calories(1).fat(1).sodium(1).carbohydrate(1).build();
         System.out.println(build);
+    }
+    @org.junit.Test
+    public void testKeySet(){
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("1","1");
+        Set<String> strings1 = hashMap.keySet();
+        hashMap.put("2","2");
+        Set<String> strings2 = hashMap.keySet();
+        Assert.assertEquals(strings1,strings2);
+        System.out.println(strings1==strings2);
     }
 }
